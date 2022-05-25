afe6b61d0636e8e36c9505a8ffd6ab8980f36401
Cristian Cavanzo <cristian.frontendev@gmail.com>
2022-05-25 06:06:20 -0500

    Stable(App Ui) Feat: -Component Composition from the components Todo Counter and TodoSearch -Create TodoHeader

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
-
 function App() {
-  return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-  );
+    return (
+        <TodoProvider>
+            <AppUI />
+        </TodoProvider>
+    );
 }
 
 export default App;
