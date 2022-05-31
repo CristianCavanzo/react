3035ed4aff616f1645a18fe68f41ae4434afa740
Cristian Cavanzo <cristian.frontendev@gmail.com>
2022-05-31 06:45:23 -0500

    Stable(index app) Feat: -Add the render props in our components

--- src/App/index.js
+++ src/App/index.js
@@ -40,12 +40,14 @@ function App() {
                 />
             </TodoHeader>
 
-            <TodoList>
-                {error && <TodosError />}
-                {loading && <TodosLoading />}
-                {!loading && !searchedTodos.length && <EmptyTodos />}
-
-                {searchedTodos.map((todo) => (
+            <TodoList
+                error={error}
+                loading={loading}
+                searchedTodos={searchedTodos}
+                onError={() => <TodosError />}
+                onLoading={() => <TodosLoading />}
+                onEmptyTodos={() => <EmptyTodos />}
+                render={(todo) => (
                     <TodoItem
                         key={todo.text}
                         text={todo.text}
@@ -53,8 +55,8 @@ function App() {
                         onComplete={() => completeTodo(todo.text)}
                         onDelete={() => deleteTodo(todo.text)}
                     />
-                ))}
-            </TodoList>
+                )}
+            />
 
             {!!openModal && (
                 <Modal>
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import './TodoList.css'
+import './TodoList.css';
 
 function TodoList(props) {
-  return (
-    <section>
-      <ul>
-        {props.children}
-      </ul>
-    </section>
-  );
+    return (
+        <section className="TodoList-container">
+            {props.error && props.onError()}
+            {props.loading && props.onLoading()}
+            {!props.loading &&
+                !props.searchedTodos?.length &&
+                props.onEmptyTodos()}
+
+            {props.searchedTodos.map(props.render)}
+
+            <ul>{props.children}</ul>
+        </section>
+    );
 }
 
 export { TodoList };
