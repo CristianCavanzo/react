0342a9a037e34b86a6d2bcee484a54fced2a7116
Cristian Cavanzo <cristian.frontendev@gmail.com>
2022-06-05 12:41:44 -0500

    Stable Feat: -HOC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
 import './index.css';
 
+function App(props) {
+  return (
+    <h1>{props.saludo}, {props.nombre}</h1>
+  )
+}
+
+function withSaludo(WrappedComponent) {
+  return function WrappedComponentWithSaludo(saludo) {
+    return function ComponenteDeVerdad(props) {
+      return (<React.Fragment>
+        <WrappedComponent {...props} saludo={saludo} />
+        <p>Estamos acompaando al WrappedComponent</p>
+      </React.Fragment>)
+    }
+  }
+}
+
+const AppWithSaludo = withSaludo(App)('hey')
 ReactDOM.render(
-  <App />,
+  <AppWithSaludo  nombre="Pepe"/>,
   document.getElementById('root')
 );
